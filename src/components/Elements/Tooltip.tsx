9b80a969a8efbcca3588f204042f975cc2405128
Salman Ibrahim <salmanibrahim.pk@gmail.com>
2023-11-21 03:07:24 +0500

    linting fixes

--- src/components/Elements/Tooltip.tsx
+++ src/components/Elements/Tooltip.tsx
@@ -1,12 +1,20 @@
 import { RefObject, createRef, useState } from "react";
 import "@/assets/css/tooltip.css";
+
+type Position = {
+  visibility: string;
+  opacity: number;
+  left?: string;
+  right?: string;
+};
+
 type Props = {
-    children: React.ReactNode,
-    text: string,
-}
+  children: React.ReactNode;
+  text: string;
+};
 
 function Tooltip(props: Props) {
-    const [isTooltipVisible, setTooltipVisible] = useState(false);
+  const [isTooltipVisible, setTooltipVisible] = useState(false);
 
   const showTooltip = () => {
     setTooltipVisible(true);
@@ -16,52 +24,49 @@ function Tooltip(props: Props) {
     setTooltipVisible(false);
   };
 
-  const calculateTooltipPosition = (buttonRef: RefObject<any>, tooltipRef: RefObject<any>) => {
-    if (!buttonRef.current || !tooltipRef.current) return {};
+  const calculateTooltipPosition = (
+    buttonRef: RefObject<HTMLSpanElement>,
+    tooltipRef: RefObject<HTMLDivElement>
+  ): Position => {
+    if (!buttonRef.current || !tooltipRef.current) return { visibility: 'hidden', opacity: 0 };
 
     const buttonRect = buttonRef.current.getBoundingClientRect();
     const tooltipRect = tooltipRef.current.getBoundingClientRect();
 
-    const position = {
+    const position: Position = {
       visibility: isTooltipVisible ? 'visible' : 'hidden',
       opacity: isTooltipVisible ? 1 : 0,
     };
 
     if (buttonRect.left + tooltipRect.width > window.innerWidth) {
-        // Tooltip goes beyond the right edge of the screen
-        // TODO: Position the tooltip to the left of the content
+      // Tooltip goes beyond the right edge of the screen
+      position.left = 'auto';
+      position.right = '0';
     } else {
-        // Default positioning
-        // TODO: Position the tooltip to the right of the content
+      // Default positioning
+      position.left =
+        buttonRect.left + window.scrollX + buttonRect.width / 2 - tooltipRect.width / 2 + 'px';
+      position.right = 'auto';
     }
 
     return position;
   };
 
-  const buttonRef = createRef();
-  const tooltipRef = createRef();
+  const buttonRef = createRef<HTMLSpanElement>();
+  const tooltipRef = createRef<HTMLDivElement>();
 
   const tooltipPosition = calculateTooltipPosition(buttonRef, tooltipRef);
 
   return (
     <div className="tooltip-container">
-      <span
-        className="tooltip-trigger"
-        onMouseOver={showTooltip}
-        onMouseOut={hideTooltip}
-        ref={buttonRef as any}
-      >
+      <span className="tooltip-trigger" onMouseOver={showTooltip} onMouseOut={hideTooltip} ref={buttonRef}>
         {props.children}
       </span>
-      <div
-        className={`tooltip`}
-        style={tooltipPosition}
-        ref={tooltipRef as any}
-      >
+      <div className={`tooltip`} style={tooltipPosition as React.CSSProperties} ref={tooltipRef}>
         {props.text}
       </div>
     </div>
   );
-};
+}
 
 export default Tooltip;
